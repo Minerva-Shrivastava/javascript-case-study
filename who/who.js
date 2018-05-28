f9ffb71eb746acae41aec93ebea752418af04f07
minerva.shrivastava <minerva.shrivastava@gmail.com>
2018-05-28 19:03:19 +0530

    who.js file updated

--- who/who.js
+++ who/who.js
@@ -1,5 +1,15 @@
-function who(){}
+// function who(){
 
+//     this.name = function(name) {
+//         if(name == undefined)
+//                  return 'One for you, one for me.';
+//              else
+//                  return 'One for '+name+', one for me.';
+//     }
+
+// }
+
+function who() {}
 who.prototype.name = function(name) {
     if(name == undefined)
         return 'One for you, one for me.';
