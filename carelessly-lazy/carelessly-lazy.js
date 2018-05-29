34b15a00afc30e0f64a5c9b9f9db154aabb51207
minerva.shrivastava <minerva.shrivastava@gmail.com>
2018-05-29 14:03:43 +0530

    carelessly-lazy assignment

--- carelessly-lazy/carelessly-lazy.js
+++ carelessly-lazy/carelessly-lazy.js
@@ -5,14 +5,13 @@ lazyGuy.prototype.hey = function(conversation) {
     var yell='Whoa, chill out!';
     var anythingElse = 'Whatever.';
 
-    var whatever_array = conversation.match(new RegExp(/[a-zA-Z-,. !'?][0-9]/g));
-    console.log(new RegExp(/[a-zA-Z-,. !'?][0-9]^[?]/g).test(conversation));
-    if(new RegExp(/[a-zA-Z-,. !'?][0-9]^[?]/g).test(conversation))
-        return anythingElse;
+    // var whatever_array = conversation.match(new RegExp(/[a-zA-Z-,. !'?][0-9]/g));
+    // console.log(new RegExp(/[a-zA-Z-,. !'?][0-9]^[?]/g).test(conversation));
+    // if(new RegExp(/[a-zA-Z-,. !'?][0-9]^[?]/g).test(conversation))
+    //     return anythingElse;
 
-    
-    var yell_array = conversation.match(new RegExp(/^[a-z][A-Z !%^*@#$()0-9]/g));
-    if(new RegExp(/^[a-z][A-Z !%^*@#$()0-9]/g).test(conversation))
+    console.log(new RegExp(/[A-Z !%^*@#$()0-9]/g).test(conversation));
+    if(new RegExp(/[A-Z !%^*@#$()0-9]/g).test(conversation))
         return yell;
     
     
